79292e43b6ac3acdb7bdc1dca81a7ab3ae409b2a
Глеб Якубец <whoyapc@MacBook-Air-Gleb.local>
2024-04-15 03:51:39 +0700

    Готовый наполовину онбординг

--- lib/features/onboardig/view/onboarding_screen_src.dart
+++ lib/features/onboardig/view/onboarding_screen_src.dart
@@ -29,7 +29,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         width: double.infinity,
         height: double.infinity,
         decoration: const BoxDecoration(
-          color: Color.fromARGB(255, 255, 255, 255),
+          color: Color(0xFF05051D),
         ),
         child: Column(
           mainAxisSize: MainAxisSize.max,
@@ -37,7 +37,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             Stack(
               children: [
                 Align(
-                  alignment: Alignment.center,
+                  alignment: const AlignmentDirectional(0, 0),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(8),
                     child: Image.asset(
@@ -48,17 +48,17 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                   ),
                 ),
                 Align(
-                  alignment: Alignment.bottomCenter,
+                  alignment: const AlignmentDirectional(0, 0),
                   child: Padding(
-                    padding: const EdgeInsets.only(bottom: 60),
+                    padding: const EdgeInsetsDirectional.fromSTEB(0, 360, 0, 0),
                     child: Text("Создайте свою сказку прямо сейчас",
                         style: theme.textTheme.textStyle),
                   ),
                 ),
                 Align(
-                  alignment: Alignment.bottomCenter,
+                  alignment: const AlignmentDirectional(0, 0),
                   child: Padding(
-                    padding: const EdgeInsets.only(bottom: 94),
+                    padding: const EdgeInsetsDirectional.fromSTEB(0, 325, 0, 0),
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(8),
                       child: const Icon(
@@ -72,7 +72,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               ],
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              padding: const EdgeInsetsDirectional.fromSTEB(22, 20, 22, 0),
               child: Column(
                 mainAxisSize: MainAxisSize.max,
                 children: [
@@ -80,8 +80,8 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                     onPressed: () async {
                       Navigator.pushNamed(context, 'LoginPage');
                     },
-                    color: Color.fromARGB(255, 155, 94, 225),
-                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    color: const Color.fromARGB(255, 155, 94, 225),
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
                     borderRadius: BorderRadius.circular(20),
                     child: Container(
                       height: 60,
@@ -98,8 +98,8 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                       onPressed: () async {
                         Navigator.pushNamed(context, 'SignUpPage');
                       },
-                      color: Color(0x1AFFFFFF),
-                      padding: EdgeInsets.symmetric(horizontal: 24),
+                      color: const Color(0x1AFFFFFF),
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
                       borderRadius: BorderRadius.circular(20),
                       child: Container(
                         height: 60,
